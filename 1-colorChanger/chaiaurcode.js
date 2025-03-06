463f75b1f42abdc08b25188c46b557568a7fd51e
wasim2451 <wasimakhtar786khan@gmail.com>
2025-03-07 04:16:59 +0530

    Changes in 1.

--- 1-colorChanger/chaiaurcode.js
+++ 1-colorChanger/chaiaurcode.js
@@ -2,5 +2,24 @@ function greyclickHandler() {
   const element = document.getElementById('grey');
   const id_name = element.id;
   console.log(id_name);
-  document.querySelector('canvas').style.background = 'grey';
+  document.querySelector('body').style.backgroundColor = id_name;
 }
+function whiteclickHandler() {
+  const element = document.getElementById('white');
+  const id_name = element.id;
+  console.log(id_name);
+  document.querySelector('body').style.backgroundColor = id_name;
+}
+function blueclickHandler() {
+  const element = document.getElementById('blue');
+  const id_name = element.id;
+  console.log(id_name);
+  document.querySelector('body').style.backgroundColor = id_name;
+}
+function yellowclickHandler() {
+  const element = document.getElementById('yellow');
+  const id_name = element.id;
+  console.log(id_name);
+  document.querySelector('body').style.backgroundColor = id_name;
+}
+
