d556825f55fa7ed2d6147abccbdc007ace4ed124
wasim2451 <wasimakhtar786khan@gmail.com>
2025-03-08 03:16:43 +0530

    commit the guess Number

--- 4-GuessTheNumber/chaiaurcode.js
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -1,32 +1,47 @@
-//select the form 
-let totalGuesses=0;
-let remaining_guesses=10;
-const form=document.querySelector('form');
-form.addEventListener('submit', (e)=>{
+//select the form
+let totalGuesses = 0;
+let remaining_guesses = 10;
+const result = document.getElementById('result');
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const random_number=Math.floor(Math.random()*100)+1;
-  console.log(random_number);
-  const guessedNumber=document.getElementById('guessField').value;
-  console.log(guessedNumber);
-  const result=document.getElementById('result')
-  if(guessedNumber==random_number){
-    result.textContent='Correct'
-
-  }else{
-    result.textContent='Wrong Guess'
+  const random_number = Math.floor(Math.random() * 100) + 1;
+  console.log('Random Number', random_number);
+  const guessedNumber = Number(document.getElementById('guessField').value);
+  if (typeof guessedNumber === 'number' && !isNaN(guessedNumber)) {
+    console.log('Guessed Number', guessedNumber);
+    if (guessedNumber == random_number) {
+      result.textContent = 'Correct';
+    } else {
+      result.textContent = 'Wrong Guess';
+    }
+    counter();
+  } else {
+    alert('Wrong Input');
   }
-  counter();
-})
-function counter(){
-  if(totalGuesses==10){
-    totalGuesses=0;
-    remaining_guesses=10;
-    alert('You can not Proceed');
+});
+function counter() {
+  totalGuesses += 1;
+  remaining_guesses -= 1;
+  if (totalGuesses == 10) {
+    const promptValue = prompt(
+      'You can not Proceed! Want a new Game ? Yes or No'
+    );
+    if (promptValue === 'Yes' || promptValue == 'yes') {
+      totalGuesses = 0;
+      remaining_guesses = 10;
+    } else {
+      alert('game Over !');
+      const temp = document.getElementById('guessField');
+      temp.value = '0';
+      temp.setAttribute('disabled', 'true');
+      const submit = document.getElementById('subt');
+      submit.setAttribute('disabled', 'true');
+      submit.value = '❌Disabled';
+    }
   }
-  totalGuesses+=1;
-  remaining_guesses-=1;
-  const guesses=document.getElementById('guesses');
-  const lastResult=document.getElementById('lastResult');
-  guesses.innerHTML=`${totalGuesses}`;
-  lastResult.innerHTML=`${remaining_guesses}`;
+  const guesses = document.getElementById('guesses');
+  const lastResult = document.getElementById('lastResult');
+  guesses.innerHTML = `${totalGuesses}`;
+  lastResult.innerHTML = `${remaining_guesses}`;
 }
