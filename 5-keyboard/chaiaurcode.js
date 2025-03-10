c15af7c2fbb6e91288e0b4b9322b345f262ef37c
wasim2451 <wasimakhtar786khan@gmail.com>
2025-03-10 14:58:19 +0530

    commit key board js

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,23 @@
 console.log('Project 5');
+console.log('Project 5');
+//In Window object add Keydown event listener
+window.addEventListener('keydown', (e) => {
+    //select id
+    const insert = document.getElementById('insert');
+    insert.innerHTML = `
+    <div class='color'> 
+    <table>
+    <tr>
+    <th>Key</th>
+    <th>KeyCode</th>
+    <th>Code</th>
+    </tr>
+    <tr>
+    <th>${e.key == ' ' ? 'Space' : e.key}</th>
+    <th>${e.keyCode}</th>
+    <th>${e.code}</th>
+    </tr>
+    </table>
+    </div>
+  `;
+});
